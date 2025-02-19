b12323a04546f8cc22dec32b33bdf3bdfdd2eb71
hhylalala <huanhuiying.hhy@gmail.com>
2025-02-19 15:28:30 +0800

    debugging lofic, if only 1 search bar, there will not be any remove button, if there is more than 1 searchbar, the remove button will existt

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,4 +1,277 @@
 
+// import React, { useState } from 'react';
+// import { Search } from 'lucide-react';
+// import { Check as CheckIcon, Close as CloseIcon } from '@mui/icons-material'; 
+// import './SearchBar.css'; // Import the CSS file
+// import SEOAuditResultTable from "./SEOAuditResultTable";
+// import { FaPlusCircle } from "react-icons/fa";
+// import Button from '@mui/material/Button';
+// import SEOCompetitorAnalysisSummaryTable from './SEOCompetitorAnalysisSummaryTable';
+
+
+//   const SearchBar = ({ onSearch }) => {
+//     const [domains, setDomains] = useState([{ domain: "", result: null }]);
+//     const [searchTriggered, setSearchTriggered] = useState(false);  // Track if search has been triggered
+
+
+//     const validateLength = (value, min, max) => value.length >= min && value.length <= max;
+
+//     const fetchSEOData = async (url) => {
+//       try {
+//           const response = await fetch(`http://localhost:8000/seo-audit/?url=${encodeURIComponent(url)}`);
+//           if (!response.ok) throw new Error(`Error: ${response.statusText}`);
+//           const data = await response.json();
+
+//           console.log("Fetched data:", data); // Check if httpsAuditResult is part of the data object
+//           if (!data.hasOwnProperty('httpsAuditResult')) {
+//               alert(`httpsAuditResult is missing for ${url}`);
+//           }
+
+//           if (data.error) {
+//               alert(`Error fetching data for ${url}: ${data.error}`);
+//               return null;
+//           }
+          
+//           console.log("Fetched data:", data);
+//           //console.log("Fetched data keys:", Object.keys(data));
+//           const httpsAudit = data.httpsAuditResult;
+//           console.log("Fetched httpdata:", httpsAudit);  // Should log "Pass"
+//           console.log("Fetched pageSpeedInsight", data.page_speed )
+        
+//           return {
+//               ...data,
+//               httpsAuditResult: data.httpsAuditResult,
+//               mobile_friendly: data.mobile_friendly,
+//               page_speed: data.page_speed,
+//               structured_data_validation: data.structured_data_validation,
+//               validation: data.validation,
+              
+//           };
+//       } catch (error) {
+//           console.error(`Error fetching data for ${url}:`, error);
+//           alert('Failed to fetch SEO data. Please try again later.');
+//           return null;
+//       }
+//   };
+
+      
+//     // Add a new search bar
+//     const handleAddSearchBar = () => {
+//       if (domains.length < 5) {
+//         setDomains([...domains, { domain: "", result: null }]);
+//       } else {
+//         alert("You can only add up to 5 search bars.");
+//       }
+//     };
+
+//     // Remove a specific search bar
+//     const handleRemoveSearchBar = (index) => {
+//       setDomains(domains.filter((_, i) => i !== index));
+//     };
+
+//     // Update a specific domain input
+//     const handleDomainChange = (index, value) => {
+//       const updatedDomains = [...domains];
+//       updatedDomains[index].domain = value;
+//       setDomains(updatedDomains);
+//     };
+
+//     const handleSearch = async () => {
+//       console.log("Domains before search:", domains);
+
+//       const emptyDomain = domains.some(entry => !entry.domain.trim());
+//       if (emptyDomain) {
+//         alert("Please fill in all domain fields before searching.");
+//         return;
+//       }
+
+//       // Mark the search as triggered, if not it will be rendered immediately, affect ui view 
+//       setSearchTriggered(true);
+    
+//     // Proceed with SEO data fetching for each domain
+//     const updatedDomains = await Promise.all(
+//       domains.map(async (entry) => {
+//         const data = await fetchSEOData(entry.domain);
+//         console.log("Fetched data for domain:", entry.domain, data);
+//         return {
+//           ...entry,
+//           result: data || null,
+//         };
+//       })
+//     );
+
+//     // Update the domains with fetched data
+//     console.log("Updated domains:", updatedDomains);
+//     setDomains(updatedDomains);
+//     //setSearchTriggered(false);  Reset search state
+//   };
+
+//   const createRows = (data) => {
+//     console.log("Creating rows for data:", data);
+//     if (!data) {
+//       return []; // Return an empty array if data is undefined or null
+//     }
+
+//     return [
+//       { label: 'HTML Type', value: data.html_type || 'Unknown', requirement: 'N/A', valid: 'N/A', recommendation: 'N/A' },
+//       //  valid: validateLength(data.title || '', 50, 60)
+//       //{ label: 'Title', value: data.title || 'No title', requirement: '50 - 60 Characters', validation: data.validation?.title?.half_valid, recommendation: 'Ensure the title is between 50 - 60 characters' },
+//       { label: 'Title', value: data.title || 'No title', requirement: '50 - 60 Characters', valid: validateLength(data.title || '', 30, 91) ? (validateLength(data.title || '', 50, 60) ? true : "partial") : false, recommendation: 'Ensure the title is between 50 - 60 characters' },
+//       //{ label: 'Meta Description', value: data.meta_description || 'No meta description', requirement: '150 - 160 Characters', valid: validateLength(data.meta_description || '', 150, 160), recommendation: 'Ensure the meta description is between 150 - 160 characters' },
+//       { label: 'Meta Description', value: data.meta_description || 'No meta description', requirement: '150 - 160 Characters', valid: validateLength(data.meta_description || '', 130, 170) ? (validateLength(data.meta_description || '', 150, 160) ? true : "partial") : false, recommendation: 'Ensure the meta description is between 150 - 160 characters' },
+//       { label: 'Canonical', value: data.canonical || 'No canonical tag', requirement: 'Point to preferred version of page to avoid duplicate content issues', valid: data.canonical !== 'No canonical tag', recommendation: 'Add a canonical tag to prevent duplicate content issues' },
+//       { label: 'Robots Meta Tag', value: data.robots || 'No robots meta tag', requirement: 'Use noindex to prevent page from being indexed & nofollow to prevent links from being followed', valid: data.robots !== 'No robots meta tag', recommendation: 'Ensure robots meta tag is properly set' },
+//       { label: 'Sitemap Status', value: data.sitemap_status || 'No sitemap', requirement: 'Submitted to Search Engine', valid: data.sitemap_status !== 'No sitemap', recommendation: 'Submit a sitemap to search engines for better crawling' },
+//       { label: 'Mobile Friendly', value: data.mobile_friendly || 'Unknown', requirement: 'Responsive & works well on mobile', valid: data.mobile_friendly === 'Mobile-friendly', recommendation: 'Ensure the site is responsive and mobile-friendly' },
+//       //{ label: 'Page Speed', value: data.page_speed || 'Unknown', requirement: 'Aim for faster loading times to improve user experience', valid: data.page_speed === 'Pass', recommendation: 'Improve page speed for better user experience' },
+//       { label: 'Page Speed', value: data.page_speed.result || 'Unknown', details: data.page_speed.details || 'No details available', valid: data.page_speed.result === 'Pass', recommendation: 'Improve page speed for better user experience' },
+//       { label: 'HTTPS Audit', value: data.httpsAuditResult || 'Unknown', requirement: 'HTTPS ensures secure communication', valid: data.httpsAuditResult === 'Pass', recommendation: 'Ensure the website uses HTTPS for secure communication' },
+      
+//        // Add a row for Structured Data
+//        { 
+//         label: 'Structured Data', 
+//         value: data.structured_data ? JSON.stringify(data.structured_data, null, 2) : 'No structured data found',
+//         requirement: 'Ensure correct structured data for SEO purposes', 
+//         valid: data.structured_data ? 'Valid' : 'Invalid', 
+//         recommendation: 'Ensure the structured data is properly implemented according to Schema.org' 
+//       },
+
+//       // Add a row for image alt text validation
+//       {
+//         label: 'Image Alt Text',
+//         value: data.image_alt_text.total_images > 0
+//             ? data.image_alt_text.missing_alt_images.length > 0
+//                 ? data.image_alt_text.missing_alt_images.map(src => `${src} → No alt text`).join(', ')
+//                 : `All ${data.image_alt_text.total_images} images have alt text`
+//             : 'No images found',
+//         requirement: 'All images should have descriptive alt text',
+//         valid: data.image_alt_text.missing_alt === 0, // Passes validation if no images are missing alt text
+//         recommendation: 'Ensure all images have meaningful alt text for accessibility and SEO'
+//     },
+
+//     // Add a row for Structured Data
+//     {
+//       label: 'Structured Data',
+//       // value: data.structured_data_validation ? JSON.stringify({
+//       //   totalValidItems: data.structured_data_validation.totalValidItems,
+//       //   // validItems: data.structuredData_Validation.validItems,
+//       //   validItemTypes: data.structured_data_validation.validItemTypes
+//       // }, null, 2) : 'No structured data found',
+//       value: data.structured_data_validation
+//       ? `Detected Valid Items: ${data.structured_data_validation.totalValidItems}\nValid Item Types: ${data.structured_data_validation.validItemTypes.join(', ')}`
+//       : 'No structured data found',
+
+//       requirement: 'Ensure correct structured data for SEO purposes',
+//       valid: data.structured_data_validation.totalValidItems > 0,
+//       recommendation: 'Ensure the structured data is properly implemented according to Schema.org. Based on the result, there are ' + data.structured_data_validation.totalValidItems + ' valid items with the types: ' + (data.structured_data_validation.validItemTypes.join(", ") || 'No valid item types found.')
+//     }
+    
+    
+//     ];
+//   };
+    
+//   const combinedRows = domains
+//     .map((entry) => {
+//       const rows = entry.result ? createRows(entry.result) : [];
+//       return rows.map((row) => ({
+//         label: row.label,
+//         domainValid: row.valid,
+//       }));
+//     })
+//     .flat();
+
+//     const combinedScores = domains.map((entry) => {
+//       if (!entry.result) return { domain: entry.domain, scores: [] };
+
+//       const rows = createRows(entry.result);
+//       const scores = rows.map((row) => ({
+//         label: row.label,
+//         valid: row.valid,
+//       }));
+
+//       return { domain: entry.domain, scores };
+//     });
+
+
+//     console.log("Combined rows:", combinedRows);
+   
+//     return (
+//       <div className="container">
+//         <div className="search-bar-form">
+//           {/* Add a flex container to arrange search bars horizontally */}
+//           <div className="search-bar-flex-container">
+//           {/* <div className={`search-bar-flex-container ${domains.length === 1 ? "centered" : ""}`}> */}
+//             {domains.map((entry, index) => (
+//               <div key={index} className="search-bar-container">
+//                 <div className="search-bar-wrapper">
+//                   <input
+//                     type="text"
+//                     value={entry.domain}
+//                     onChange={(e) => handleDomainChange(index, e.target.value)}
+//                     placeholder={`Enter domain ${index + 1}`}
+//                     className="search-bar-input"
+//                   />
+//                   <Button
+//                     sx={{
+//                       position: "absolute",
+//                       right: "20px",
+//                       top: "50%",
+//                       transform: "translateY(-50%)",
+//                       minWidth: "50px",
+//                       height: "50%",
+//                       borderRadius: "5px",
+//                     }}
+//                     variant="outlined"
+//                     color="secondary"
+//                     onClick={() => handleRemoveSearchBar(index)}
+//                   >
+//                     Remove
+//                   </Button>
+//                 </div>
+//               </div>
+//             ))}
+//           </div>
+    
+//           <Button 
+//             variant="contained" 
+//             onClick={handleAddSearchBar} 
+//             sx={{
+//               margin: "20px 0px 20px 0px"
+//             }}>
+//             Add Search Bar
+//           </Button>
+//           <Button variant="contained" onClick={handleSearch}>
+//             Search
+//           </Button>
+//         </div>
+    
+//         {/* Competitor Summary Table */}
+//         <div className="scrollable-table-container">
+//           {searchTriggered && combinedScores.length > 0 && (
+//             <SEOCompetitorAnalysisSummaryTable data={combinedScores} />
+//           )}
+//         </div>
+    
+//         <br />
+    
+//         {domains.map((entry, index) => (
+//           <div key={index} className="scrollable-table-container">
+//               {entry.result && <SEOAuditResultTable rows={createRows(entry.result)} domain={entry.domain} />}
+//           </div>
+//         ))}
+    
+//         <br />
+//       </div>
+//     );
+    
+//   };
+
+//   export default SearchBar;      
+
+// above is working version
+
+
+
 import React, { useState } from 'react';
 import { Search } from 'lucide-react';
 import { Check as CheckIcon, Close as CloseIcon } from '@mui/icons-material'; 
@@ -200,6 +473,7 @@ import SEOCompetitorAnalysisSummaryTable from './SEOCompetitorAnalysisSummaryTab
         <div className="search-bar-form">
           {/* Add a flex container to arrange search bars horizontally */}
           <div className="search-bar-flex-container">
+          {/* <div className={`search-bar-flex-container ${domains.length === 1 ? "centered" : ""}`}> */}
             {domains.map((entry, index) => (
               <div key={index} className="search-bar-container">
                 <div className="search-bar-wrapper">
@@ -210,6 +484,7 @@ import SEOCompetitorAnalysisSummaryTable from './SEOCompetitorAnalysisSummaryTab
                     placeholder={`Enter domain ${index + 1}`}
                     className="search-bar-input"
                   />
+                  {domains.length > 1 && (
                   <Button
                     sx={{
                       position: "absolute",
@@ -226,6 +501,7 @@ import SEOCompetitorAnalysisSummaryTable from './SEOCompetitorAnalysisSummaryTab
                   >
                     Remove
                   </Button>
+                  )}
                 </div>
               </div>
             ))}
@@ -266,6 +542,11 @@ import SEOCompetitorAnalysisSummaryTable from './SEOCompetitorAnalysisSummaryTab
   };
 
   export default SearchBar;      
+
+
+
+
+
 
  // return (
     //   <div className="container">
